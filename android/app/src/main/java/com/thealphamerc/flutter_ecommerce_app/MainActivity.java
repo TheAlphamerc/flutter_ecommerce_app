f226324b907bbfb56d388be67cf18b1e1e20f18f
Sonu Sharma <sonu.sharma045@gmail.com>
2020-02-27 20:50:16 +0530

    initial commit

---
+++ android/app/src/main/java/com/thealphamerc/flutter_ecommerce_app/MainActivity.java
@@ -0,0 +1,13 @@
+package com.thealphamerc.flutter_ecommerce_app;
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
+    GeneratedPluginRegistrant.registerWith(flutterEngine);
+  }
+}
