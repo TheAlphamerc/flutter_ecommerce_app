253daf3cab41d636514ef1231b1cba30e5db9e16
SHIVANI GUPTA <shivanigupta.stp2015@gmail.com>
2020-08-08 10:36:41 +0530

    issue solved by shivani

--- lib/src/config/route.dart
+++ lib/src/config/route.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_ecommerce_app/src/pages/mainPage.dart';
-import 'package:flutter_ecommerce_app/src/pages/product_detail.dart';
 
-class Routes{
-  static Map<String,WidgetBuilder> getRoute(){
-    return  <String, WidgetBuilder>{
-          '/': (_) => MainPage(),
-          // '/detail': (_) => ProductDetailPage()
-        };
+class Routes {
+  static Map<String, WidgetBuilder> getRoute() {
+    return <String, WidgetBuilder>{
+      '/': (_) => MainPage(),
+      // '/detail': (_) => ProductDetailPage()
+    };
   }
 }
