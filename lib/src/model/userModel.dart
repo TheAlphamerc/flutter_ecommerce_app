f81aa00d066e488cadfb6eb1059c9264dbb643e8
shukill <56451088+shukill@users.noreply.github.com>
2020-09-30 23:50:30 +0530

    Create userModel.dart

---
+++ lib/src/model/userModel.dart
@@ -0,0 +1,10 @@
+class AppUser{
+  String userId;
+  String userName ;
+  String userEmail ;
+  String userProfileurl ;
+AppUser({this.userId,this.userName, this.userEmail,this.userProfileurl});
+
+}
+
+
